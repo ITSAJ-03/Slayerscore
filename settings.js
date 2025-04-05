d08ab1408b4468aa44eb2b9a70bb858e2886c384
ITSAJ-03 <ajmonsta03@gmail.com>
2025-04-05 19:32:03 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
+  packname: 'MONSTA',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "AJ'S PET",
+  botOwner: 'AJMon_STA', // Your name
+  ownerNumber: '233534657872', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
